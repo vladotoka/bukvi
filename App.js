369602bea8f85d25f807398494728330a8369bb9
vladimir <vladimir.k.terziev@tutanota.comgit config --global user.name vladimirgit config --global user.email vladimir.k.terziev@tutanota.com>
2026-03-11 20:20:12 +0200

    blueprint

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Link } from '@react-navigation/native';
+import { Button } from '@react-navigation/elements';
+import { Calendar } from './Calendar';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>А́ а́ Ъ́ ъ́ О́ о́ У́ у́ е́ Е́ И́ и́ Ю́ ю́ Я́ я́</Text>
+      <Link screen="Calendar">Календар</Link>
+      <Button screen="UselessInfo">Знание</Button>
+    </View>
+  );
+}
+
+function UselessInfo() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Списък книги от Рик и Морти 6 х1</Text>
+      <Text>"Барнс и Нобъл".</Text>
+      <Text>"Четири споразумения",</Text>
+      <Text>148</Text>
+      <Text>'00:10:32,798 -- `{'>'}` 00:10:35,634'</Text>
+      <Text>"Яж, моли се и обичай",</Text>
+      <Text>'"Черният рицар се завръща".'</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: { backgroundColor: 'aquamarine' },
+       }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Начало' }} />
+      <Stack.Screen name="Calendar" component={Calendar} options={{ title: 'Календар' }} />
+      <Stack.Screen name="UselessInfo" component={UselessInfo} options={{ title: 'Полезно знание' }} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   );
 }
 
---
+++ Calendar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function Calendar() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Днес е 7531 година</Text>
+    </View>
+  );
+}
