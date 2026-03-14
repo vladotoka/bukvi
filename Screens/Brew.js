0a61ae935fa5172e69ca3e55d18300910baa3aad
vladimir <vladimir.k.terziev@tutanota.comgit config --global user.name vladimirgit config --global user.email vladimir.k.terziev@tutanota.com>
2026-03-14 19:03:46 +0200

    keyfield

--- Screens/Brew.js
+++ Screens/Brew.js
@@ -1,16 +1,27 @@
- import React from 'react';
- import { View, Text } from 'react-native';
- import { OpenURLButton } from '../Components/OpenURLButton';
- //import APIkey from Async storage
- 
- 
- export function Brew() {
-   return (
-     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-       <Text>brewdata here</Text>
-       <OpenURLButton url={'https://breww.com/dashboard'}>https://breww.com</OpenURLButton>
-     </View>
-   );
- }
- 
- // get request https://breww.com/api/
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+import { OpenURLButton } from "../Components/OpenURLButton";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+//import APIkey from Async storage
+
+export function Brew() {
+  const [keyField, setKeyField] = useState("");
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem("my-key", value);
+    } catch (e) {
+      // saving error
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>brewdata here</Text>
+      <OpenURLButton url={"https://breww.com/dashboard"}>
+        https://breww.com
+      </OpenURLButton>
+    </View>
+  );
+}
+
+// get request https://breww.com/api/
