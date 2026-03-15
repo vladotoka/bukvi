a456c34808b1d089434e10433c79e1828cfb1608
vladotoka <vladimir.k.terziev@tutanota.com>
2026-03-15 19:47:30 +0200

    settings mockup

---
+++ Screens/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export function Settings() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>'безначѣльнъ'</Text>
+            <Text>BREW API key</Text>
+            <Text>meteo API key</Text>
+
+        </View>
+    );
+}
